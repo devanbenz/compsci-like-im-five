bb26efdde4b7ed8a00c27978fa51995a077d8d70
devanbenz <devandbenz@gmail.com>
2023-05-28 12:18:47 -0500

    feat: rm required var

--- src/env.mjs
+++ src/env.mjs
@@ -10,7 +10,7 @@ export const env = createEnv({
     NODE_ENV: z.enum(["development", "test", "production"]),
     NEXTAUTH_SECRET:
       process.env.NODE_ENV === "production"
-        ? z.string().min(1)
+        ? z.string().min(1).optional()
         : z.string().min(1).optional(),
     NEXTAUTH_URL: z.preprocess(
       // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
